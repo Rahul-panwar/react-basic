6d66ee852c8a706e67e8d6196ad4fc2521a059c6
Rahul-panwar <rahulpanwar1205@gmail.com>
2020-05-05 18:22:54 +0530

    Name change using props and state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
+const App = props => {
+  const [personsState, setPersonsState] = useState({
+      persons: [
+        { name: "Naren", age: "27" },
+        { name: "Satyam", age: "25" },
+        { name: "Parth", age: "24" }
+      ]
+    });
+
+    const [otherState, setOtherState] = useState('Some Other Value');
+    // console.log(personsState, otherState);
+
+  const switchNameHandler = (newName) => {
+    setPersonsState({
+      persons: [
+        { name: newName, age: "27" },
+        { name: "Satyam", age: "26" },
+        { name: "Parth", age: "24" }
+      ]
+    })
+  }
+
+  // usernameChangedHandler = (event) => {
+  //   this.setState({
+  //     persons: [
+  //       { name: "Naren", age: "27" },
+  //       { name: event.target.value, age: "26" },
+  //       { name: "Parth", age: "24" }
+  //     ]
+  //   })
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi I am a react app.</h1>
+      <p>Hello World! let's play.</p>
+
+      <button onClick={() => switchNameHandler('maxmilian')}>Click Here</button>
+
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person 
+        name={personsState.persons[1].name} 
+        age={personsState.persons[1].age} 
+        click={switchNameHandler.bind(this, 'Naren')}
+         >Hello this extra</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
     </div>
   );
 }
+// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I\'m React app') );
 
 export default App;
